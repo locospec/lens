9313ff761f17dab67b5d0f574dd8a8efbfcd06f1
ShreyDhyani <shrey@betalectic.com>
2025-01-10 16:51:18 +0530

    doc: changed log for button message

--- packages/elements-react/lib/components/Button/index.tsx
+++ packages/elements-react/lib/components/Button/index.tsx
@@ -2,6 +2,6 @@ import styles from "./styles.module.css";
 
 export function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   const { className, ...restProps } = props;
-  console.log("Loading Button in Elements-react MODIFIED LOG MESSAGE 4");
+  console.log("Loading Button in Elements-react MODIFIED LOG MESSAGE 5");
   return <button className={`${className} ${styles.button}`} {...restProps} />;
 }
