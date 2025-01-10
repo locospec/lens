99419cfa6ada36c844138a221c7e99f4de70c76e
ShreyDhyani <shrey@betalectic.com>
2025-01-10 20:23:14 +0530

    chore: Added sample log

--- packages/blueprint-react/lib/components/Button/index.tsx
+++ packages/blueprint-react/lib/components/Button/index.tsx
@@ -2,6 +2,6 @@ import styles from "./styles.module.css";
 
 export function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   const { className, ...restProps } = props;
-  console.log("Loading lib-a Button in this page in alpha 2");
+  console.log("Loading lib-a Button in this page in alpha  sample log");
   return <button className={`${className} ${styles.button}`} {...restProps} />;
 }
