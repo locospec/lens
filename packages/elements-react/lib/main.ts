442e13ab3dd13b75cfb74027636aac1ed3c66500
ShreyDhyani <shrey@betalectic.com>
2025-01-10 10:50:51 +0530

    feat: Added checkbox export to main

--- packages/elements-react/lib/main.ts
+++ packages/elements-react/lib/main.ts
@@ -2,3 +2,4 @@ export { Button } from "./components/Button";
 export { Input } from "./components/Input";
 export { Label } from "./components/Label";
 export { Field } from "./components/Field";
+export { Checkbox } from "./components/Checkbox";
