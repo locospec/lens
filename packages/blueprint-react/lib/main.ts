7f67f3aab9b54ef08f723a9a49d5dc585f065d89
Rajiv Seelam <rajiv@betalectic.com>
2025-01-11 11:49:31 +0530

    feat: add Avatar component and update Button component with new variants

--- packages/blueprint-react/lib/main.ts
+++ packages/blueprint-react/lib/main.ts
@@ -2,3 +2,4 @@ export { Button } from "./components/Button";
 export { Input } from "./components/Input";
 export { Label } from "./components/Label";
 export { Field } from "./components/Field";
+export { Avatar, AvatarImage, AvatarFallback } from "./components/Avatar";
