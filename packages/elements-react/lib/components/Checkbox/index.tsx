b3d747ab7396f799a08bd799cfcbcc4c966e6608
ShreyDhyani <shrey@betalectic.com>
2025-01-13 15:29:32 +0530

    fix: modified checkbox log

--- packages/elements-react/lib/components/Checkbox/index.tsx
+++ packages/elements-react/lib/components/Checkbox/index.tsx
@@ -5,7 +5,7 @@ import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import { Check } from "lucide-react";
 import { cn } from "../utils/cn";
 
-console.log(">>> Sample Commit in checkbox component for testing #7");
+console.log(">>> Sample Commit in checkbox component for testing #8");
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
