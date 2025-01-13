899c68aa8ee9da5dd004d61b5b49b39a654d0ee5
ShreyDhyani <shrey@betalectic.com>
2025-01-13 13:47:30 +0530

    patch: Added console.log for button

--- packages/elements-react/lib/components/Button/index.tsx
+++ packages/elements-react/lib/components/Button/index.tsx
@@ -2,6 +2,6 @@ import styles from "./styles.module.css";
 
 export function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   const { className, ...restProps } = props;
-  console.log("Loading Button in Elements-react MODIFIED LOG MESSAGE 28");
+  console.log("Loading Button in Elements-react MODIFIED LOG MESSAGE 29");
   return <button className={`${className} ${styles.button}`} {...restProps} />;
 }
