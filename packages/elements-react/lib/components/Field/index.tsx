45bb6f32ff61a1ec23eb2a5f757bbb399f96c5d2
ShreyDhyani <shrey@betalectic.com>
2025-01-13 15:30:01 +0530

    patch: modified field log

--- packages/elements-react/lib/components/Field/index.tsx
+++ packages/elements-react/lib/components/Field/index.tsx
@@ -8,7 +8,7 @@ type FormFieldProps = {
 };
 
 export const Field = ({ label, placeholder }: FormFieldProps) => {
-  console.log("Loading elements-react Field in this page #3");
+  console.log("Loading elements-react Field in this page #4");
 
   const [inputCustomCountValue, setInputCustomCountValue] = useState("");
 
