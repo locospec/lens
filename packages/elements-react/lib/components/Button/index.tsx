1af805863167eec1b5cb5622969ae4c4b9defeb8
ShreyDhyani <shrey@betalectic.com>
2025-01-13 13:41:04 +0530

    patch: Should release patch version

--- packages/elements-react/lib/components/Button/index.tsx
+++ packages/elements-react/lib/components/Button/index.tsx
@@ -2,6 +2,6 @@ import styles from "./styles.module.css";
 
 export function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   const { className, ...restProps } = props;
-  console.log("Loading Button in Elements-react MODIFIED LOG MESSAGE 27");
+  console.log("Loading Button in Elements-react MODIFIED LOG MESSAGE 28");
   return <button className={`${className} ${styles.button}`} {...restProps} />;
 }
