6285a1b1479d88aa2580cb6c3bdf688eddec3190
Rajiv Seelam <rajiv@betalectic.com>
2025-01-16 16:24:46 +0530

    feat: wrap IFramePreview and ChildPreview components in a div for improved layout structure

--- packages/react-component-preview/lib/components/PreviewWrapper.tsx
+++ packages/react-component-preview/lib/components/PreviewWrapper.tsx
@@ -60,77 +60,75 @@ export function PreviewWrapper({ children, className }: PreviewWrapperProps) {
   }, [maxWidth]);
 
   return (
-    <div className="twp">
-      <div className="grid w-full gap-4">
-        <div className="flex items-center justify-between mr-[12px]">
-          <div className="py-2 text-xs">
-            Width: {width}px (
-            {resizablePanelRef.current && resizablePanelRef.current.getSize()}%)
-            [{getBreakpoint(width)?.title}]
-          </div>
-
-          {/* {JSON.stringify(availableBreakpoints)} */}
-
-          <div className="hidden h-7 items-center gap-1.5 rounded-md border p-[2px] shadow-none lg:flex">
-            <TooltipProvider>
-              <ToggleGroup
-                type="single"
-                defaultValue="100"
-                onValueChange={(value) => {
-                  if (resizablePanelRef?.current) {
-                    resizablePanelRef.current.resize(parseInt(value));
-                  }
-                }}
-                className="flex items-center"
-              >
-                {availableBreakpoints.map((breakpoint: Breakpoint) => {
-                  const Icon = breakpoint.icon;
-                  return (
-                    <ToggleGroupItem
-                      data-state={
-                        getBreakpoint(width)?.title === breakpoint.title
-                          ? "on"
-                          : "off"
-                      }
-                      value={breakpoint?.percentage?.toString() || "0"}
-                      key={breakpoint.title}
-                      className="h-[22px] w-[22px] min-w-0 rounded-sm p-0"
-                      title={`${breakpoint.title} (${breakpoint.minWidthPx}px)`}
-                    >
-                      <Tooltip>
-                        <TooltipTrigger asChild>
-                          <Icon className="h-3.5 w-3.5" />
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          <p>
-                            `${breakpoint.title} (${breakpoint.minWidthPx}px)`
-                          </p>
-                        </TooltipContent>
-                      </Tooltip>
-
-                      {/* <span className="text-xs">{breakpoint.title}</span> */}
-                    </ToggleGroupItem>
-                  );
-                })}
-              </ToggleGroup>
-            </TooltipProvider>
-          </div>
+    <div className="grid w-full gap-4">
+      <div className="flex items-center justify-between mr-[12px]">
+        <div className="py-2 text-xs">
+          Width: {width}px (
+          {resizablePanelRef.current && resizablePanelRef.current.getSize()}%) [
+          {getBreakpoint(width)?.title}]
         </div>
 
-        <ResizablePanelGroup
-          direction="horizontal"
-          className="relative z-10 bg-gray-50"
-        >
-          <ResizablePanel
-            ref={resizablePanelRef}
-            className={`border ${className}`}
-          >
-            <div ref={panelContentRef}>{children}</div>
-          </ResizablePanel>
-          <ResizableHandle className="relative hidden w-3 bg-transparent p-0 after:absolute after:right-0 after:top-1/2 after:h-8 after:w-[6px] after:-translate-y-1/2 after:translate-x-[-1px] after:rounded-full after:bg-border after:transition-all after:hover:h-10 md:block" />
-          <ResizablePanel defaultSize={0} minSize={0} />
-        </ResizablePanelGroup>
+        {/* {JSON.stringify(availableBreakpoints)} */}
+
+        <div className="hidden h-7 items-center gap-1.5 rounded-md border p-[2px] shadow-none lg:flex">
+          <TooltipProvider>
+            <ToggleGroup
+              type="single"
+              defaultValue="100"
+              onValueChange={(value) => {
+                if (resizablePanelRef?.current) {
+                  resizablePanelRef.current.resize(parseInt(value));
+                }
+              }}
+              className="flex items-center"
+            >
+              {availableBreakpoints.map((breakpoint: Breakpoint) => {
+                const Icon = breakpoint.icon;
+                return (
+                  <ToggleGroupItem
+                    data-state={
+                      getBreakpoint(width)?.title === breakpoint.title
+                        ? "on"
+                        : "off"
+                    }
+                    value={breakpoint?.percentage?.toString() || "0"}
+                    key={breakpoint.title}
+                    className="h-[22px] w-[22px] min-w-0 rounded-sm p-0"
+                    title={`${breakpoint.title} (${breakpoint.minWidthPx}px)`}
+                  >
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <Icon className="h-3.5 w-3.5" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>
+                          `${breakpoint.title} (${breakpoint.minWidthPx}px)`
+                        </p>
+                      </TooltipContent>
+                    </Tooltip>
+
+                    {/* <span className="text-xs">{breakpoint.title}</span> */}
+                  </ToggleGroupItem>
+                );
+              })}
+            </ToggleGroup>
+          </TooltipProvider>
+        </div>
       </div>
+
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="relative z-10 bg-gray-50"
+      >
+        <ResizablePanel
+          ref={resizablePanelRef}
+          className={`border ${className}`}
+        >
+          <div ref={panelContentRef}>{children}</div>
+        </ResizablePanel>
+        <ResizableHandle className="relative hidden w-3 bg-transparent p-0 after:absolute after:right-0 after:top-1/2 after:h-8 after:w-[6px] after:-translate-y-1/2 after:translate-x-[-1px] after:rounded-full after:bg-border after:transition-all after:hover:h-10 md:block" />
+        <ResizablePanel defaultSize={0} minSize={0} />
+      </ResizablePanelGroup>
     </div>
   );
 }
