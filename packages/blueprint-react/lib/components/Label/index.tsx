6ea42212e73f9c78a0e1ebe2cfc1f0f0587b6af4
Rajiv Seelam <rajiv@betalectic.com>
2025-01-17 18:48:34 +0530

    chore: remove not user thing

--- packages/blueprint-react/lib/components/Label/index.tsx
+++ packages/blueprint-react/lib/components/Label/index.tsx
@@ -3,11 +3,8 @@ import styles from "./styles.module.css";
 export function Label(props: React.LabelHTMLAttributes<HTMLLabelElement>) {
   const { className, ...restProps } = props;
 
-  console.log("Blueprint Label");
-
   return (
     <>
-      <h1>Hello....</h1>
       <label
         className={`${styles.label} bp:bg-orange-400 font-medium ${className}`}
         {...restProps}
