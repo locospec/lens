8fc8154c3e15b58e5b05c22573c876178d22734c
ShreyDhyani <shrey@betalectic.com>
2025-01-17 11:54:40 +0530

    docs(lens): add browser only story

---
+++ apps/site/browser/LensSample.tsx
@@ -0,0 +1,32 @@
+import { Lens } from "@locospec/elements-react";
+import React, { useState, useEffect } from "react";
+import { makeServer } from "../src/server/mocks/mirageServer";
+
+const LensSample = () => {
+  useEffect(() => {
+    makeServer();
+  }, []);
+  const [checkedIds, setCheckedIds] = useState([]);
+
+  const handleSelectionChange = (selectedItem) => {
+    if (selectedItem) {
+      setCheckedIds(selectedItem);
+    } else {
+      setCheckedIds([]);
+    }
+  };
+
+  return (
+    <Lens
+      selectionType={"multiple"}
+      configEndpoint="/api/data-bench/test-data/config"
+      dataEndpoint="/api/data-bench/test-data/fetch"
+      onSelect={(value) => {
+        handleSelectionChange(value);
+      }}
+      selectedItems={checkedIds}
+    />
+  );
+};
+
+export default LensSample;
--- apps/site/src/pages/lens.tsx
+++ apps/site/src/pages/lens.tsx
@@ -1,34 +1,20 @@
-// import { Lens } from "@locospec/elements-react";
-import React, { useState } from "react";
-// import { makeServer } from "../server/mocks/mirageServer";
+"use client";
+
+import React from "react";
+import BrowserOnly from "@docusaurus/BrowserOnly";
+
+const LensSample = React.lazy(() => import("../../browser/LensSample"));
 
 const LensStory = () => {
-  // useEffect(() => {
-  //   makeServer();
-  // }, []);
-  const [checkedIds, setCheckedIds] = useState([]);
-
-  const handleSelectionChange = (selectedItem) => {
-    if (selectedItem) {
-      setCheckedIds(selectedItem);
-    } else {
-      setCheckedIds([]);
-    }
-  };
-
   return (
     <div className="flex flex-col p-4 h-screen items-center w-full gap-y-4">
       <h3 className="font-bold text-lg">Table with Multi Select </h3>
       <div className="flex-grow overflow-y-auto border-2 max-h-[500px] w-full">
-        {/* <Lens
-          selectionType={"multiple"}
-          configEndpoint="/api/data-bench/test-data/config"
-          dataEndpoint="/api/data-bench/test-data/fetch"
-          onSelect={(value) => {
-            handleSelectionChange(value);
+        <BrowserOnly>
+          {() => {
+            return <LensSample />;
           }}
-          selectedItems={checkedIds}
-        /> */}
+        </BrowserOnly>
       </div>
     </div>
   );
