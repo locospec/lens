2e85c4638bd92884ef41c8b86a53493dd70bdbc2
ShreyDhyani <shrey@betalectic.com>
2025-01-17 10:58:31 +0530

    feat: Added knip package and changed import style for testing '

--- packages/elements-react/lib/main.ts
+++ packages/elements-react/lib/main.ts
@@ -3,4 +3,17 @@ export { Input } from "./components/Input";
 export { Label } from "./components/Label";
 export { Field } from "./components/Field";
 export { Checkbox } from "./components/Checkbox";
-export { Lens } from "./components/table";
+export {
+  useTableConfig,
+  List,
+  ListData,
+  ResizeHandle,
+  Lens,
+  TableBody,
+  MemoizedTableBody,
+  TableCell,
+  TableHeader,
+  TableHeaderSection,
+  TableMetrics,
+  TableRow,
+} from "./components/table";
