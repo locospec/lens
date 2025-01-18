8138886b02911baca1b2f2fa8d9b69bb0ae483a9
Rajiv Seelam <rajiv@betalectic.com>
2025-01-18 13:27:47 +0530

    refactor(blueprint-react): simplify TableExample component structure and enhance layout

--- packages/blueprint-react/src/App.tsx
+++ packages/blueprint-react/src/App.tsx
@@ -20,37 +20,47 @@ import { Theme, ShellSidebar } from "../lib/main";
 
 function TableExample() {
   return (
-    <Card asChild variant="classic" size="4">
-      <Table.Root style={{ backgroundColor: "var(--gray-a1)" }}>
-        <Table.Header
-          style={{
-            backgroundColor: "var(--gray-a2)",
-            borderTop: "1px solid var(--gray-a5)",
-          }}
-        >
-          <Table.Row>
-            <Table.ColumnHeaderCell>Full name</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Email</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Group</Table.ColumnHeaderCell>
+    <Table.Root style={{ backgroundColor: "var(--gray-a1)" }}>
+      <Table.Header
+        style={{
+          backgroundColor: "var(--gray-a2)",
+          borderTop: "1px solid var(--gray-a5)",
+        }}
+      >
+        <Table.Row>
+          <Table.ColumnHeaderCell>Full name</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell>Email</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell>Group</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell>Group</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell>Group</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell>Group</Table.ColumnHeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <Table.Row key={index}>
+            <Table.RowHeaderCell>
+              <Skeleton>User {index + 1} Lorem Ipsum</Skeleton>
+            </Table.RowHeaderCell>
+            <Table.Cell>
+              <Skeleton>user{index + 1}@workos.com</Skeleton>
+            </Table.Cell>
+            <Table.Cell>
+              <Skeleton>Role {index + 1}</Skeleton>
+            </Table.Cell>
+            <Table.Cell>
+              <Skeleton>Role {index + 1}</Skeleton>
+            </Table.Cell>
+            <Table.Cell>
+              <Skeleton>Role {index + 1}</Skeleton>
+            </Table.Cell>
+            <Table.Cell>
+              <Skeleton>Role {index + 1}</Skeleton>
+            </Table.Cell>
           </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {Array.from({ length: 5 }).map((_, index) => (
-            <Table.Row key={index}>
-              <Table.RowHeaderCell>
-                <Skeleton>User {index + 1} Lorem Ipsum</Skeleton>
-              </Table.RowHeaderCell>
-              <Table.Cell>
-                <Skeleton>user{index + 1}@workos.com</Skeleton>
-              </Table.Cell>
-              <Table.Cell>
-                <Skeleton>Role {index + 1}</Skeleton>
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
-    </Card>
+        ))}
+      </Table.Body>
+    </Table.Root>
   );
 }
 
@@ -125,11 +135,21 @@ function App() {
   return (
     <Theme>
       <ShellSidebar>
-        <Container size="1" className="bp-bg-gray-200">
-          <Section>
-            <TableExample />
-          </Section>
-        </Container>
+        <TableExample />
+        <Box
+          style={{
+            height: "100%",
+            backgroundColor: "var(--gray-a3)",
+            backgroundClip: "padding-box",
+            border: "1px solid var(--gray-a5)",
+            borderRadius: "var(--radius-1)",
+            backgroundImage: `url("data:image/svg+xml,%3Csvg width='6' height='6' viewBox='0 0 6 6' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%239C92AC' fill-opacity='0.2' fill-rule='evenodd'%3E%3Cpath d='M5 0h1L0 6V5zM6 5v1H5z'/%3E%3C/g%3E%3C/svg%3E")`,
+          }}
+        ></Box>
+
+        <Section size={"2"}>
+          <TableExample />
+        </Section>
       </ShellSidebar>
     </Theme>
   );
