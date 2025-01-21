469c1463d9c9a4f8ce122e0c58f4437285af59cf
ShreyDhyani <107391802+ShreyDhyani@users.noreply.github.com>
2025-01-21 13:13:38 +0530

    Lens initial (#98)
    * patch(ci): version number change
    
    * patch(lens): add sample commnet
    
    * feat: modified tailwind configuration
    
    * ci(elements-lens): add package to workspace
    
    * feat(lens): add mirage server and lens example
    
    * feat(lens): modify styles for Lens component
    
    * patch(elements-layout): wrapp in layout with max-width
    
    * patch(checkbox): remove comment
    
    * patch(lens): remove inused variable
    
    * patch(lens): add sample console.log for logging result from API API call when required
    
    * feat: modified tailwind configuration
    
    * ci(elements-lens): add package to workspace
    
    * feat(lens): add mirage server and lens example
    
    * feat(lens): modify styles for Lens component
    
    * patch(elements-layout): wrapp in layout with max-width
    
    * patch(checkbox): remove comment
    
    * patch(lens): remove inused variable
    
    * patch(lens): add sample console.log for logging result from API API call when required

--- packages/elements-react/lib/components/table/Lens.tsx
+++ packages/elements-react/lib/components/table/Lens.tsx
@@ -2,7 +2,6 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import "./css/divTable.css";
 import { List } from "./List.tsx";
 import type { SelectionType } from "./interfaces/index.ts";
 
--- packages/elements-react/lib/components/table/TableHeader.tsx
+++ packages/elements-react/lib/components/table/TableHeader.tsx
@@ -3,18 +3,16 @@ import { ResizeHandle } from "./ResizeHandle.tsx";
 
 export interface TableHeaderInterface {
   headerGroup: any;
-  columnSizeVars: any;
 }
 
 // This was removed as it was unused columnSizeVars
-const TableHeader = ({ headerGroup, columnSizeVars }: TableHeaderInterface) => {
-  console.log(">>>>> columnSizeVars", columnSizeVars);
+const TableHeader = ({ headerGroup }: TableHeaderInterface) => {
   return (
-    <div key={headerGroup.id} className="flex">
+    <div key={headerGroup.id} className="le-flex">
       {headerGroup.headers.map((header: any) => (
         <div
           key={header.id}
-          className="relative border-b border-gray-200 bg-gray-100 px-4 py-1.5 text-left font-semibold"
+          className="le-relative le-border-b le-border-gray-200 le-bg-gray-100 le-px-4 le-py-1.5 le-text-left le-font-semibold"
           style={{
             width: `calc(var(--header-${header?.id}-size) * 1px)`,
           }}
