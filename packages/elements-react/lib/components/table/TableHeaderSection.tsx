83c8f2e39807a58322834d62ec909a0cf9d7e017
ShreyDhyani <shrey@betalectic.com>
2025-01-21 13:03:16 +0530

    patch(lens): remove inused variable

--- packages/elements-react/lib/components/table/TableHeaderSection.tsx
+++ packages/elements-react/lib/components/table/TableHeaderSection.tsx
@@ -5,19 +5,11 @@ export interface TableHeaderSectionInterface {
   columnSizeVars: any;
 }
 
-const TableHeaderSection = ({
-  table,
-  columnSizeVars,
-}: TableHeaderSectionInterface) => {
-  console.log("TableHeaderSection added smaple comment #1");
+const TableHeaderSection = ({ table }: TableHeaderSectionInterface) => {
   return (
     <div className="le-sticky le-top-0 le-z-10 le-bg-white">
       {table.getHeaderGroups().map((headerGroup: any) => (
-        <TableHeader
-          key={headerGroup.id}
-          headerGroup={headerGroup}
-          columnSizeVars={columnSizeVars}
-        />
+        <TableHeader key={headerGroup.id} headerGroup={headerGroup} />
       ))}
     </div>
   );
