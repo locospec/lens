af30f701e25fae56cc93d6105d2fa3230293be84
ShreyDhyani <shrey@betalectic.com>
2025-01-21 11:00:39 +0530

    patch(lens): add sample commnet

--- packages/elements-react/lib/components/table/TableHeaderSection.tsx
+++ packages/elements-react/lib/components/table/TableHeaderSection.tsx
@@ -9,6 +9,7 @@ const TableHeaderSection = ({
   table,
   columnSizeVars,
 }: TableHeaderSectionInterface) => {
+  console.log("TableHeaderSection added smaple comment #1");
   return (
     <div className="sticky top-0 z-10 bg-white">
       {table.getHeaderGroups().map((headerGroup: any) => (
