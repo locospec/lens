64a4386b7787a7dd56c977f023205014914100b8
ShreyDhyani <shrey@betalectic.com>
2025-01-22 17:28:14 +0530

    chore: modifiy cn util to work with prefix

--- packages/elements-react/lib/components/utils/cn.ts
+++ packages/elements-react/lib/components/utils/cn.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { extendTailwindMerge } from "tailwind-merge";
+
+const customTwMerge = extendTailwindMerge({ prefix: "le-" });
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return customTwMerge(clsx(inputs));
 }
