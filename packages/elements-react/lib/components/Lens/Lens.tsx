d20bb42b3aa74096e539a3a46a10f5f59c5acd3c
ShreyDhyani <shrey@betalectic.com>
2025-01-22 16:09:20 +0530

    feat: add lens context to allow better handling of props

--- packages/elements-react/lib/components/Lens/Lens.tsx
+++ packages/elements-react/lib/components/Lens/Lens.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { List } from "./List.tsx";
 import type { SelectionType } from "./interfaces/index.ts";
+import { LensProvider } from "./context/LensContext.tsx";
 
 const queryClient = new QueryClient();
 
@@ -15,6 +16,7 @@ export interface LensInterface {
   selectedItems?: string[];
   dataEndpoint: string;
   showDevTools?: boolean;
+  showTableMetrics?: boolean;
 }
 
 const Lens = ({
@@ -24,17 +26,23 @@ const Lens = ({
   dataEndpoint,
   showDevTools = false,
   configCallback,
+  showTableMetrics = false,
 }: LensInterface) => {
   return (
     <QueryClientProvider client={queryClient}>
       {showDevTools ? <ReactQueryDevtools /> : <></>}
-      <List
-        onSelect={onSelect}
-        selectedItems={selectedItems}
-        configEndpoint={configEndpoint || ""}
-        dataEndpoint={dataEndpoint}
-        configCallback={configCallback}
-      />
+      <LensProvider
+        showDevTools={showDevTools}
+        showTableMetrics={showTableMetrics}
+      >
+        <List
+          onSelect={onSelect}
+          selectedItems={selectedItems}
+          configEndpoint={configEndpoint || ""}
+          dataEndpoint={dataEndpoint}
+          configCallback={configCallback}
+        />
+      </LensProvider>
     </QueryClientProvider>
   );
 };
---
+++ packages/elements-react/lib/components/Lens/context/LensContext.tsx
@@ -0,0 +1,24 @@
+import { createContext, useContext, ReactNode } from "react";
+
+interface LensContextType {
+  showDevTools?: boolean;
+  showTableMetrics?: boolean;
+}
+
+const LensContext = createContext<LensContextType | undefined>(undefined);
+
+export const useLensContext = (): LensContextType => {
+  const context = useContext(LensContext);
+  if (!context) {
+    throw new Error("useLensContext must be used within a LensProvider");
+  }
+  return context;
+};
+
+interface LensProviderProps extends LensContextType {
+  children: ReactNode;
+}
+
+export const LensProvider = ({ children, ...props }: LensProviderProps) => {
+  return <LensContext.Provider value={props}>{children}</LensContext.Provider>;
+};
