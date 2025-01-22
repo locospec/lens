dda48a8c5dbd76ecc5986d18c55a5b30dd3ed79c
ShreyDhyani <shrey@betalectic.com>
2025-01-22 12:48:50 +0530

    refactor: add resize observer as hook

--- packages/elements-react/lib/components/Lens/hooks/index.ts
+++ packages/elements-react/lib/components/Lens/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useFetchConfig } from "./src/useFetchConfig";
 export { default as useTableConfig } from "./src/useTableConfig";
+export { useResizeObserver } from "./src/useResizeObserver";
