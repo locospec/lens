8c530b2f2dfccbb28d77792dd2af54049155d332
ShreyDhyani <shrey@betalectic.com>
2025-01-22 13:15:45 +0530

    refactor: add custom hook for infinite scroll pagination with exposed callback function

--- packages/elements-react/lib/components/Lens/hooks/index.ts
+++ packages/elements-react/lib/components/Lens/hooks/index.ts
@@ -2,5 +2,6 @@ export { useFetchConfig } from "./src/useFetchConfig";
 export { useTableConfig } from "./src/useTableConfig";
 export { useResizeObserver } from "./src/useResizeObserver";
 export { useColumnSizing } from "./src/useColumnSizing";
+export { useFetchMoreOnScroll } from "./src/useFetchMoreOnScroll";
 
 export type { ResizeObserverCallback } from "./src/useResizeObserver";
