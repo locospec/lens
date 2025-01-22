e4b049fb9aef6eb07db624a1f608f2947b898cd8
ShreyDhyani <shrey@betalectic.com>
2025-01-22 12:38:58 +0530

    refactor: modify topbar(bulk action, view) and sidebar sheet

---
+++ packages/elements-react/lib/components/Lens/LensBulkActionsbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LensBulkActionsbar: React.FC = () => {
+  return (
+    <div className="le-h-10 le-bg-gray-100 le-flex le-items-center le-text-lg le-font-bold">
+      Bulk Actions Bar
+    </div>
+  );
+};
+
+export default LensBulkActionsbar;
