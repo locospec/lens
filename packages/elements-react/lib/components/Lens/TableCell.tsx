bde2c43cc32043f233bb8e11de97db8e7443517c
ShreyDhyani <shrey@betalectic.com>
2025-01-22 15:06:30 +0530

    chore: refactor table cell

--- packages/elements-react/lib/components/Lens/TableCell.tsx
+++ packages/elements-react/lib/components/Lens/TableCell.tsx
@@ -1,13 +1,20 @@
 import { flexRender } from "@tanstack/react-table";
+import type { Cell } from "@tanstack/react-table";
 
-const TableCell = ({ cell }: any) => {
+export interface TableCellProps {
+  cell: Cell<any, unknown>;
+}
+
+const TableCell = ({ cell }: TableCellProps) => {
+  const width = {
+    width: `calc(var(--col-${cell.column.id}-size) * 1px)`,
+  };
+
   return (
     <div
       className="le-truncate le-border-b le-border-gray-100 le-px-4 le-py-2"
       key={cell.id}
-      style={{
-        width: `calc(var(--col-${cell.column.id}-size) * 1px)`,
-      }}
+      style={width}
     >
       {flexRender(cell.column.columnDef.cell, cell.getContext())}
     </div>
