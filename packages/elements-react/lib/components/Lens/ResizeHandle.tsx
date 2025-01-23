eb56d6b99ab1c07a5d5ce117a8537d0b4f7d37ea
ShreyDhyani <shrey@betalectic.com>
2025-01-23 10:53:36 +0530

    patch: refactor and add type interface

--- packages/elements-react/lib/components/Lens/ResizeHandle.tsx
+++ packages/elements-react/lib/components/Lens/ResizeHandle.tsx
@@ -1,14 +1,22 @@
-"use client";
+import type { Header } from "@tanstack/react-table";
+import { cn } from "../utils/cn";
+
+export interface ResizeHandleProps {
+  header: Header<any, unknown>;
+  isResizing: boolean;
+}
 
 const ResizeHandle = ({ header, isResizing }: any) => {
+  const resizeHandle = header.getResizeHandler();
   return (
     <div
       onDoubleClick={() => header.column.resetSize()}
-      onMouseDown={header.getResizeHandler()}
-      onTouchStart={header.getResizeHandler()}
-      className={`le-absolute le-right-0 le-top-0 le-h-full le-w-1 le-cursor-col-resize le-touch-none le-select-none le-bg-gray-200 hover:le-bg-gray-400 ${
-        isResizing ? "le-bg-blue-500" : ""
-      }`}
+      onMouseDown={resizeHandle}
+      onTouchStart={resizeHandle}
+      className={cn(
+        "le-absolute le-right-0 le-top-0 le-h-full le-w-1 le-cursor-col-resize le-touch-none le-select-none le-bg-gray-200 hover:le-bg-gray-400",
+        isResizing && "le-bg-blue-500"
+      )}
     />
   );
 };
