ad6a3f23ec5f01264f35f21248e307c17b4d8b00
ShreyDhyani <shrey@betalectic.com>
2025-01-23 15:27:30 +0530

    chore: added twp class to wrapper div

--- packages/elements-react/lib/components/Lens/LoadingState.tsx
+++ packages/elements-react/lib/components/Lens/LoadingState.tsx
@@ -1,5 +1,5 @@
 const LoadingState = () => (
-  <div className="le-w-full le-h-full le-flex le-items-center le-justify-center le-border">
+  <div className="twp le-w-full le-h-full le-flex le-items-center le-justify-center le-border le-rounded-lg">
     <label className="le-text-2xl le-font-semibold">Loading Table.......</label>
   </div>
 );
