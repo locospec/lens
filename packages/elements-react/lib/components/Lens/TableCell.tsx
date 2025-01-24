cd4595836353e03377c99a27d94862907eefd0fc
ShreyDhyani <shrey@betalectic.com>
2025-01-24 11:36:54 +0530

    patch: add conditional rendering of border for last element in a table cell

--- packages/elements-react/lib/components/Lens/TableCell.tsx
+++ packages/elements-react/lib/components/Lens/TableCell.tsx
@@ -1,18 +1,23 @@
 import { flexRender } from "@tanstack/react-table";
 import type { Cell } from "@tanstack/react-table";
+import { cn } from "../utils/cn";
 
 export interface TableCellProps {
   cell: Cell<any, unknown>;
+  isLast: boolean;
 }
 
-const TableCell = ({ cell }: TableCellProps) => {
+const TableCell = ({ cell, isLast = false }: TableCellProps) => {
   const width = {
     width: `calc(var(--col-${cell.column.id}-size) * 1px)`,
   };
 
   return (
     <div
-      className="le-truncate le-px-4 le-py-2 le-border-r le-border-[var(--gray-7)]"
+      className={cn(
+        "le-truncate le-px-4 le-py-2",
+        isLast ? "" : "le-border-r le-border-[var(--gray-7)]"
+      )}
       key={cell.id}
       style={width}
     >
