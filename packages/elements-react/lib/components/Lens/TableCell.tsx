45ac8c5bb0faad84632fdb667dbd9fbaa65d428f
ShreyDhyani <shrey@betalectic.com>
2025-01-24 19:03:44 +0530

    feat: add css controlled sizing

--- packages/elements-react/lib/components/Lens/TableCell.tsx
+++ packages/elements-react/lib/components/Lens/TableCell.tsx
@@ -18,6 +18,7 @@ const TableCell = ({ cell }: TableCellProps) => {
         "le-truncate le-px-4 le-py-2 ",
         "le-bg-[var(--gray-1)] group-hover:le-bg-[var(--gray-3)]",
         "le-border-r le-border-b le-border-[var(--gray-7)]",
+        "le-p-[var(--table-cell-padding)] le-min-h-[var(--table-cell-min-height)]",
         isSelected && "le-bg-[var(--gray-3)]"
       )}
       key={cell.id}
