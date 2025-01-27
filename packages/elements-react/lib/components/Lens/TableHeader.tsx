20a7365e295f83b5b6b7c95930c1a533f6abee98
ShreyDhyani <shrey@betalectic.com>
2025-01-27 12:35:45 +0530

    feat: add conditional rendering for resize handler and modify styles for table header

--- packages/elements-react/lib/components/Lens/TableHeader.tsx
+++ packages/elements-react/lib/components/Lens/TableHeader.tsx
@@ -7,30 +7,37 @@ export interface TableHeaderInterface {
   headerGroup: HeaderGroup<any>;
 }
 
-//le-border-[var(--gray-9)] le-bg-[var(--gray-4)]
 const TableHeader = ({ headerGroup }: TableHeaderInterface) => {
   return (
     <div key={headerGroup.id} className="le-flex ">
-      {headerGroup.headers.map((header) => (
-        <div
-          key={header.id}
-          className={cn(
-            "le-relative le-border-b le-border-r le-border-[var(--gray-7)] le-text-left le-font-semibold",
-            "le-p-[var(--table-cell-padding)] le-min-h-[var(--table-cell-min-height)]"
-          )}
-          style={{
-            width: `calc(var(--header-${header?.id}-size) * 1px)`,
-          }}
-        >
-          {header.isPlaceholder
-            ? null
-            : flexRender(header.column.columnDef.header, header.getContext())}
-          <ResizeHandle
-            header={header}
-            isResizing={header.column.getIsResizing()}
-          />
-        </div>
-      ))}
+      {headerGroup.headers.map((header) => {
+        const id = header?.id;
+        const addResizeHandler =
+          header.column.columnDef.minSize !== header.column.columnDef.maxSize;
+
+        return (
+          <div
+            key={header.id}
+            className={cn(
+              "le-relative le-border-b le-border-r le-text-left le-font-semibold",
+              "le-p-[var(--table-cell-padding)] le-min-h-[var(--table-cell-min-height)] le-border-[var(--gray-7)]"
+            )}
+            style={{
+              width: `calc(var(--header-${id}-size) * 1px)`,
+            }}
+          >
+            {header.isPlaceholder
+              ? null
+              : flexRender(header.column.columnDef.header, header.getContext())}
+            {addResizeHandler && (
+              <ResizeHandle
+                header={header}
+                isResizing={header.column.getIsResizing()}
+              />
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 };
