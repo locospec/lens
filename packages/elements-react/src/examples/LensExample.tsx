47223d591d8db502ffd996096af5890fb75c8875
ShreyDhyani <shrey@betalectic.com>
2025-01-27 15:11:31 +0530

    docs: add components for lens table display

--- packages/elements-react/src/examples/LensExample.tsx
+++ packages/elements-react/src/examples/LensExample.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { Section } from "@radix-ui/themes";
 import { Lens } from "../../lib/main";
 import { makeServer } from "../mocks/mirageServer";
+import SectionWrapper from "../helpers/SectionWrapper";
 
 const LensExample = () => {
   React.useEffect(() => {
@@ -65,10 +65,15 @@ const LensExample = () => {
 
   return (
     <div className="le-mx-auto">
-      <h2 className="le-text-2xl le-font-bold le-mb-4 ">
-        Lens Sample Table SIZE 1
-      </h2>
-      <Section className="le-h-[600px] le-bg-red-300 le-px-4 le-mb-10" size="1">
+      <SectionWrapper
+        spacing="le-mx-4 le-mb-10"
+        heading="Lens Sample Table SIZE 1 Variant Ghost"
+        backgroundColor="le-bg-red-200"
+        // backgroundImage="https://img.freepik.com/free-vector/background-crystal-geometric-colorful-abstract_343694-2894.jpg?t=st=1737966773~exp=1737970373~hmac=8f561c1029760db0cd37a2ae82a81c76891babea7496d1cba6bb153554716e75&w=2000"
+        // backgroundImage="https://img.freepik.com/free-photo/colorful-background-with-alcohol-ink_24972-1282.jpg?t=st=1737967216~exp=1737970816~hmac=e9cf8210dd9a0d4dc7c1b0ed5801a9691edb9cbd77d912e53e20265108fa4e4f&w=1800"
+        // backgroundImage="https://img.freepik.com/free-vector/hand-drawn-butterfly-outline-background_52683-67985.jpg?t=st=1737967374~exp=1737970974~hmac=d4778bd4c65d5708fef2e7886ca381feca49da7e863ef8d27dc071af87a37120&w=2000"
+        // backgroundImage="https://img.freepik.com/free-vector/blue-speed-comic-style-background_23-2148820924.jpg?t=st=1737967182~exp=1737970782~hmac=f0adc1a146ed69ea8cd94b65aa899c07c7e84f8dbde8db391f0d23ad2b59e06e&w=2000"
+      >
         <Lens
           configEndpoint="/api/data-bench/auction-data/config"
           dataEndpoint="/api/data-bench/auction-data/fetch"
@@ -76,13 +81,16 @@ const LensExample = () => {
             handleSelectionChange(value);
           }}
           selectedItems={checkedIds}
-          // showTableMetrics
+          variant="ghost"
         />
-      </Section>
-      <h2 className="le-text-2xl le-font-bold le-mb-4 ">
-        Lens Sample Table SIZE 2
-      </h2>
-      <Section className="le-h-[200px] le-px-4 le-mb-10" size="1">
+      </SectionWrapper>
+      <SectionWrapper
+        heading="Lens Sample Table SIZE 2 Variant Surface"
+        backgroundColor="le-bg-white"
+        spacing="le-mx-4 le-p-0"
+        // backgroundImage="https://img.freepik.com/free-vector/background-crystal-geometric-colorful-abstract_343694-2894.jpg?t=st=1737966773~exp=1737970373~hmac=8f561c1029760db0cd37a2ae82a81c76891babea7496d1cba6bb153554716e75&w=2000"
+        // backgroundImage="https://img.freepik.com/free-vector/blue-speed-comic-style-background_23-2148820924.jpg?t=st=1737967182~exp=1737970782~hmac=f0adc1a146ed69ea8cd94b65aa899c07c7e84f8dbde8db391f0d23ad2b59e06e&w=2000"
+      >
         <Lens
           configEndpoint="/api/data-bench/auction-data/config"
           dataEndpoint="/api/data-bench/auction-data/fetch"
@@ -90,37 +98,10 @@ const LensExample = () => {
             handleSelectionChange(value);
           }}
           selectedItems={checkedIds}
+          variant="surface"
           size="2"
         />
-      </Section>
-      <h2 className="le-text-2xl le-font-bold le-mb-4 ">
-        Lens Sample Table SIZE 3
-      </h2>
-      <Section className="le-h-[200px] le-px-4 le-mb-10" size="1">
-        <Lens
-          configEndpoint="/api/data-bench/auction-data/config"
-          dataEndpoint="/api/data-bench/auction-data/fetch"
-          onSelect={(value) => {
-            handleSelectionChange(value);
-          }}
-          selectedItems={checkedIds}
-          size="3"
-        />
-      </Section>
-      {/* <Section className="le-h-[700px] le-mt-10" size="1">
-        <h2 className="le-text-2xl le-font-bold le-mb-4">
-          Lens Sample Table 2
-        </h2>
-        <Lens
-          onSelect={(value) => {
-            handleSelectionChange2(value);
-          }}
-          configCallback={returnTableConfiguration}
-          dataCallback={returnTableData}
-          selectedItems={checkedIds2}
-          //   showTableMetrics
-        />
-      </Section> */}
+      </SectionWrapper>
     </div>
   );
 };
