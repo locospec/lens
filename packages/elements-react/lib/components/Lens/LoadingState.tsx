f62cc1cff4c711b7ef70a6fc2a8b13e7edd25f24
ShreyDhyani <shrey@betalectic.com>
2025-01-27 15:27:19 +0530

    feat: add variant styles for loading state component

--- packages/elements-react/lib/components/Lens/LoadingState.tsx
+++ packages/elements-react/lib/components/Lens/LoadingState.tsx
@@ -1,7 +1,20 @@
-const LoadingState = () => (
-  <div className="twp le-w-full le-h-full le-flex le-items-center le-justify-center le-border le-rounded-lg">
-    <label className="le-text-2xl le-font-semibold">Loading Table.......</label>
-  </div>
-);
+import { cn } from "../utils/cn";
+import { useLensContext } from "./context/LensContext";
+
+const LoadingState = () => {
+  const { variantClass } = useLensContext();
+  return (
+    <div
+      className={cn(
+        variantClass,
+        "twp le-w-full le-h-full le-flex le-items-center le-justify-center le-border le-rounded-lg le-table-loading"
+      )}
+    >
+      <label className="le-text-2xl le-font-semibold">
+        Loading Table.......
+      </label>
+    </div>
+  );
+};
 
 export default LoadingState;
