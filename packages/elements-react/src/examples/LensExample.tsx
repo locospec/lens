f926f268b839e540ea56c22814623fdd92c01bbd
ShreyDhyani <shrey@betalectic.com>
2025-01-28 17:30:13 +0530

    docs: add test sample for testing actionsand columns alignment

--- packages/elements-react/src/examples/LensExample.tsx
+++ packages/elements-react/src/examples/LensExample.tsx
@@ -42,7 +42,7 @@ const LensExample = () => {
     <div className="le-mx-0">
       <SectionWrapper
         heading="Lens Sample Table SIZE 3 Variant Surface"
-        backgroundColor="le-bg-yellow-400 dark:bg-black"
+        backgroundColor="le-bg-white dark:bg-black"
         // backgroundVideoUrl={Video}
         // backgroundVideoUrl="../assets/hyperbola_landing_page_video.webm"
       >
