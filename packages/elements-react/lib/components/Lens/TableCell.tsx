c61bddbd78b4372b2c8e05d572bf4cedb2f627ac
ShreyDhyani <shrey@betalectic.com>
2025-01-28 17:26:39 +0530

    feat: add support for custom align ment for each column cell

--- packages/elements-react/lib/components/Lens/TableCell.tsx
+++ packages/elements-react/lib/components/Lens/TableCell.tsx
@@ -10,15 +10,20 @@ const TableCell = ({ cell }: TableCellProps) => {
   const width = {
     width: `calc(var(--col-${cell.column.id}-size) * 1px)`,
   };
+
+  const align = (cell.column.columnDef.meta as any)?.align;
+  const align_class = align ? `le-justify-${align}` : "";
   const isSelected = cell.row.getIsSelected();
 
   return (
     <div
       className={cn(
         "le-table-cell",
-        "le-truncate le-px-4 le-py-2 ",
+        "le-truncate le-px-4 le-py-2",
         "le-p-[var(--table-cell-padding)] le-min-h-[var(--table-cell-min-height)]",
-        "group-hover:le-bg-[var(--gray-a2)]"
+        "group-hover:le-bg-[var(--gray-a2)]",
+        align_class,
+        cell.column.id === "actions" && "le-gap-x-2"
       )}
       key={cell.id}
       style={width}
