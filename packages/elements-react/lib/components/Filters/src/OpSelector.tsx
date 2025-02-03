1179b44dae6170f3df949971a36ee6703053c2c1
ShreyDhyani <shrey@betalectic.com>
2025-02-03 10:41:19 +0530

    chore: add use case for WHERE label

--- packages/elements-react/lib/components/Filters/src/OpSelector.tsx
+++ packages/elements-react/lib/components/Filters/src/OpSelector.tsx
@@ -19,27 +19,32 @@ const OPDisplay = ({
   group,
   handleGroupOperatorChange,
 }: OPDisplayProps) => {
+  if (index === 0) {
+    return (
+      <label className={cn("le-p-1 le-w-[70px] le-text-center")}>WHERE</label>
+    );
+  }
+  if (index === 1) {
+    return (
+      <Select onValueChange={handleGroupOperatorChange}>
+        <SelectTrigger className="le-p-1 le-w-[70px] le-text-center">
+          <SelectValue placeholder={group.op.toUpperCase()} />
+        </SelectTrigger>
+        <SelectContent>
+          {GROUP_OPERATORS.map((op) => (
+            <SelectItem key={op.value} value={op.value}>
+              {op.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    );
+  }
+
   return (
-    <>
-      {index === 0 ? (
-        <Select onValueChange={handleGroupOperatorChange}>
-          <SelectTrigger className="le-p-1 le-w-[70px] le-text-center">
-            <SelectValue placeholder={group.op.toUpperCase()} />
-          </SelectTrigger>
-          <SelectContent>
-            {GROUP_OPERATORS.map((op) => (
-              <SelectItem key={op.value} value={op.value}>
-                {op.label}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-      ) : (
-        <label className={cn("le-p-1 le-w-[70px] le-text-center")}>
-          {group.op.toUpperCase()}
-        </label>
-      )}
-    </>
+    <label className={cn("le-p-1 le-w-[70px] le-text-center")}>
+      {group.op.toUpperCase()}
+    </label>
   );
 };
 
