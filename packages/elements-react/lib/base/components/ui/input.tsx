986c7bea424bab65e5998d008a199c1d36b32e68
ShreyDhyani <shrey@betalectic.com>
2025-02-03 10:40:59 +0530

    chore: modify input coponent'

--- packages/elements-react/lib/base/components/ui/input.tsx
+++ packages/elements-react/lib/base/components/ui/input.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 
 import { cn } from "@/base/lib/utils";
 
@@ -8,7 +8,8 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
       <input
         type={type}
         className={cn(
-          "le-flex le-h-9 le-w-full le-rounded-md le-border le-border-input le-bg-background hover:le-bg-accent le-px-3 le-py-1 le-text-base le-shadow-sm le-transition-colors file:le-border-0 file:le-bg-transparent file:le-text-sm file:le-font-medium file:le-text-foreground placeholder:le-text-muted-foreground focus-visible:le-outline-none focus-visible:le-ring-1 focus-visible:le-ring-ring disabled:le-cursor-not-allowed disabled:le-opacity-50 md:le-text-sm",
+          "le-flex le-h-9 le-w-full le-rounded-md le-border le-border-input le-bg-background hover:le-bg-accent le-px-3 le-py-1 le-text-base le-shadow-sm le-transition-colors placeholder:le-text-muted-foreground focus-visible:le-outline-none focus-visible:le-ring-1 focus-visible:le-ring-ring disabled:le-cursor-not-allowed disabled:le-opacity-50 md:le-text-sm",
+          "file:le-border-0 file:le-bg-transparent file:le-text-sm file:le-font-medium file:le-text-foreground",
           className
         )}
         ref={ref}
