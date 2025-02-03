36685e9d11e1d283cc4bfa47b46e98bdee4f6e45
ShreyDhyani <shrey@betalectic.com>
2025-02-03 11:51:17 +0530

    chore: add theme provider for component as a wrapper

--- packages/elements-react/lib/components/ThemeProvider/ThemeProvider.tsx
+++ packages/elements-react/lib/components/ThemeProvider/ThemeProvider.tsx
@@ -3,25 +3,14 @@ import React from "react";
 
 export interface ThemeProviderProps {
   children: React.ReactNode;
-  scaling?: string;
-  radius?: string;
 }
 
-const ThemeProvider = ({
-  children,
-  scaling = "1",
-  radius = "2",
-}: ThemeProviderProps) => {
-  if (!(+scaling < 2 && +scaling > 0)) {
-    console.error("Invalid scaling");
-    return <div>This is invalid scaling</div>;
-  }
-  if (!(+radius <= 2 && +radius > 0)) {
-    console.error("Invalid radius");
-    return <div>This is invalid radius</div>;
-  }
-
-  return <Theme>{children}</Theme>;
+const ThemeProvider = ({ children }: ThemeProviderProps) => {
+  return (
+    <Theme asChild className="lens-root" appearance="dark">
+      {children}
+    </Theme>
+  );
 };
 
 export default ThemeProvider;
