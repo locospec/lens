1587b0971cb1f69a6bbfe5e71c39902b2c71bd21
ShreyDhyani <shrey@betalectic.com>
2025-02-05 14:04:01 +0530

    chore: added pinning sample for datatable

--- packages/elements-react/lib/components/Lens/TableCell.tsx
+++ packages/elements-react/lib/components/Lens/TableCell.tsx
@@ -1,6 +1,7 @@
 import { flexRender } from "@tanstack/react-table";
 import type { Cell } from "@tanstack/react-table";
 import { cn } from "../utils/cn";
+import { getCommonPinningStyles } from "./hooks/src/getColumnPinningStyles";
 
 export interface TableCellProps {
   cell: Cell<any, unknown>;
@@ -10,6 +11,9 @@ const TableCell = ({ cell }: TableCellProps) => {
   const width = {
     width: `calc(var(--col-${cell.column.id}-size) * 1px)`,
   };
+  const column = cell.column;
+
+  const css = getCommonPinningStyles(column);
 
   const align = (cell.column.columnDef.meta as any)?.align;
   const align_class = align ? `le-justify-${align}` : "";
@@ -28,13 +32,14 @@ const TableCell = ({ cell }: TableCellProps) => {
         "le-truncate le-px-4 le-py-2",
         "le-p-[var(--table-cell-padding)] le-min-h-[var(--table-cell-min-height)]",
         "group-hover:le-bg-[var(--gray-a2)]",
-        "le-border-b le-border-[var(--gray-7)]",
+        "le-border-b le-border-[var(--gray-7)] data-[state=selected]:le-bg-[var(--gray-a2)]",
         align_class,
         cell.column.id === "actions" && "le-flex le-gap-x-2",
         cell.column.id === "actions" && align_class_block
       )}
       key={cell.id}
-      style={width}
+      style={{ ...width, ...css }}
+      // style={{ ...width }}
       data-state={isSelected && "selected"}
     >
       {flexRender(cell.column.columnDef.cell, cell.getContext())}
