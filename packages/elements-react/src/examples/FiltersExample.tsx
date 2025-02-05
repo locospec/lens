2472ea4fd91b1933b1c8a6e17fd1947547f2ca5c
ShreyDhyani <shrey@betalectic.com>
2025-02-05 16:49:22 +0530

    chore: added example doc

--- packages/elements-react/src/examples/FiltersExample.tsx
+++ packages/elements-react/src/examples/FiltersExample.tsx
@@ -2,16 +2,14 @@ import { FilterBuilder } from "../../lib/components/Filters";
 
 const FiltersExample = () => {
   return (
-    <div className="le-mx-0">
-      <div className="le-mt-2 le-w-[98vw]">
-        <FilterBuilder
-          label={"Filters surface 1"}
-          variant="classic"
-          size="2"
-          maxDepth={2}
-          // showFilterJSON={false}
-        />
-      </div>
+    <div className="le-w-11/12 le-mt-10 le-mx-auto">
+      <FilterBuilder
+        label={"Filters surface 1"}
+        variant="classic"
+        size="2"
+        maxDepth={2}
+        // showFilterJSON={false}
+      />
     </div>
   );
 };
