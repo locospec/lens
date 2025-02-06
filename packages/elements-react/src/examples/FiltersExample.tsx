923d4a3a65aaae4e002237f4d757c5b48997fbbb
ShreyDhyani <shrey@betalectic.com>
2025-02-06 12:33:15 +0530

    chore: new op added

--- packages/elements-react/src/examples/FiltersExample.tsx
+++ packages/elements-react/src/examples/FiltersExample.tsx
@@ -1,16 +1,36 @@
 import { FilterBuilder } from "../../lib/components/Filters";
-import { AttributeOption } from "../../lib/components/Filters/src/types";
+import { AttributeDefinitionMapType } from "../../lib/components/Filters/src/interfaces";
 
 const FiltersExample = () => {
-  const attributes: AttributeOption[] = [
-    { label: "First Name", value: "f_name", type: "string" },
-    { label: "Last Name", value: "l_name", type: "string" },
-    { label: "DOB", value: "date_of_birth", type: "date" },
-    { label: "Age", value: "age", type: "number" },
-  ];
+  const attributes: AttributeDefinitionMapType = {
+    f_name: {
+      label: "First Name",
+      type: "string",
+    },
+    l_name: {
+      label: "Last Name",
+      type: "string",
+    },
+    date_of_birth: {
+      label: "DOB",
+      type: "date",
+    },
+    age: {
+      label: "Age",
+      type: "number",
+    },
+    category: {
+      label: "Category",
+      type: "enum",
+    },
+    availability: {
+      label: "Availablity",
+      type: "boolean",
+    },
+  };
 
   return (
-    <div className="le-w-11/12 le-mt-10 le-mx-auto le-bg-red-100">
+    <div className="le-w-11/12 le-mt-10 le-mx-auto">
       <FilterBuilder
         label={"Filters surface 1"}
         variant="classic"
