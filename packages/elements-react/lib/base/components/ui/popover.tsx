b619e823654413e0686baeab07aca9e0527bce26
ShreyDhyani <shrey@betalectic.com>
2025-02-06 17:01:34 +0530

    chore: modify Popover to handle containerRef

--- packages/elements-react/lib/base/components/ui/popover.tsx
+++ packages/elements-react/lib/base/components/ui/popover.tsx
@@ -10,22 +10,29 @@ const PopoverAnchor = PopoverPrimitive.Anchor;
 
 const PopoverContent = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
->(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
-  <PopoverPrimitive.Portal>
-    <PopoverPrimitive.Content
-      ref={ref}
-      align={align}
-      sideOffset={sideOffset}
-      className={cn(
-        "le-lens-wrapper le-z-50 le-w-72 le-rounded-md le-border le-bg-popover le-p-4 le-text-popover-foreground le-shadow-md le-outline-none data-[state=open]:le-animate-in data-[state=closed]:le-animate-out data-[state=closed]:le-fade-out-0 data-[state=open]:le-fade-in-0 data-[state=closed]:le-zoom-out-95 data-[state=open]:le-zoom-in-95 data-[side=bottom]:le-slide-in-from-top-2 data-[side=left]:le-slide-in-from-right-2 data-[side=right]:le-slide-in-from-left-2 data-[side=top]:le-slide-in-from-bottom-2",
-        "rt-r-size-1",
-        className
-      )}
-      {...props}
-    />
-  </PopoverPrimitive.Portal>
-));
+  React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content> & {
+    containerRef?: any;
+  }
+>(
+  (
+    { className, align = "center", sideOffset = 4, containerRef, ...props },
+    ref
+  ) => (
+    <PopoverPrimitive.Portal container={containerRef?.current}>
+      <PopoverPrimitive.Content
+        ref={ref}
+        align={align}
+        sideOffset={sideOffset}
+        className={cn(
+          "le-lens-wrapper le-z-50 le-w-72 le-rounded-md le-border le-bg-popover le-p-4 le-text-popover-foreground le-shadow-md le-outline-none data-[state=open]:le-animate-in data-[state=closed]:le-animate-out data-[state=closed]:le-fade-out-0 data-[state=open]:le-fade-in-0 data-[state=closed]:le-zoom-out-95 data-[state=open]:le-zoom-in-95 data-[side=bottom]:le-slide-in-from-top-2 data-[side=left]:le-slide-in-from-right-2 data-[side=right]:le-slide-in-from-left-2 data-[side=top]:le-slide-in-from-bottom-2",
+          "rt-r-size-1",
+          className
+        )}
+        {...props}
+      />
+    </PopoverPrimitive.Portal>
+  )
+);
 PopoverContent.displayName = PopoverPrimitive.Content.displayName;
 
 export { Popover, PopoverTrigger, PopoverContent, PopoverAnchor };
