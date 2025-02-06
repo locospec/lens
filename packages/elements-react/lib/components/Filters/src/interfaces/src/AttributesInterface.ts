e357018631f4a2fa947a777e0219b264fb2c0ec2
ShreyDhyani <shrey@betalectic.com>
2025-02-06 11:25:23 +0530

    chore: add type defination for attributes

---
+++ packages/elements-react/lib/components/Filters/src/interfaces/src/AttributesInterface.ts
@@ -0,0 +1,19 @@
+export type AttributeTypes = "string" | "number" | "boolean" | "date" | "enum";
+
+export interface AttributeDefinitionType {
+  label: string;
+  type: AttributeTypes;
+}
+
+export type AttributeDefinitionMapType = {
+  [key: string]: AttributeDefinitionType;
+};
+
+export interface OptionItemType {
+  label: string;
+  value: string;
+}
+export interface AttributeOptionItemType extends OptionItemType {
+  type: AttributeTypes;
+}
+export type AttributeOptionsArrayType = AttributeOptionItemType[];
