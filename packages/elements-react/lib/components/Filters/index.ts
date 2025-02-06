47cc05e0d27cb86bbc388617721b3c0923cbc1e6
ShreyDhyani <shrey@betalectic.com>
2025-02-06 12:30:54 +0530

    chore: clean up exports

--- packages/elements-react/lib/components/Filters/index.ts
+++ packages/elements-react/lib/components/Filters/index.ts
@@ -8,11 +8,7 @@ export { default as FilterGroupComponent } from "./src/FilterGroup";
 export type { FilterGroupProps } from "./src/FilterGroup";
 
 export type {
-  Operator,
-  GroupOperator,
   Condition as FilterCondition,
   FilterGroup,
   OperatorOption,
 } from "./src/types";
-
-export { CONDITION_OPERATORS, GROUP_OPERATORS } from "./src/types";
