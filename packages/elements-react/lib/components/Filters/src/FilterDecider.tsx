1c093b7c5b19b19d2262236e753ce388ef6d6937
ShreyDhyani <shrey@betalectic.com>
2025-02-12 10:59:03 +0530

    chore: remove console.log for props

--- packages/elements-react/lib/components/Filters/src/FilterDecider.tsx
+++ packages/elements-react/lib/components/Filters/src/FilterDecider.tsx
@@ -2,7 +2,6 @@ import SimpleFilter from "./SimpleFilter";
 import FilterBuilder from "./FilterBuilder";
 
 const FilterDecider = ({ ...props }: any) => {
-  console.log(props);
   if (props?.asSimpleFilters || false) {
     return <SimpleFilter {...props} />;
   }
