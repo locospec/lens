1afee5a1aa07d89a727885ed6ce2c53b77412668
ShreyDhyani <shrey@betalectic.com>
2025-02-18 17:18:09 +0530

    chore: refactor and cleanup unrequired data

--- packages/lens-react/lib/components/LensProvider/LensProvider.tsx
+++ packages/lens-react/lib/components/LensProvider/LensProvider.tsx
@@ -1,10 +1,7 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState } from "react";
 import type { LensContextType, LensProviderProps } from "./types";
 import { useFetchConfig } from "../Lens/hooks";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import useFilterFunctions from "./hooks/useFilterFunction";
-import { useInfiniteFetch } from "./hooks/useInfiniteFetch";
-import { getProcessedFilters } from "./utils";
 
 const queryClient = new QueryClient();
 
@@ -16,16 +13,10 @@ export const LensProviderBase: React.FC<LensProviderProps> = ({
   lensConfiguration,
   children,
 }) => {
-  const { endpoint, permissionHeaders = {} } = lensConfiguration;
-  const [data, setData] = useState<any[]>([]);
-  // const [config, setConfig] = useState<any>();
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { endpoint } = lensConfiguration;
   const [error, setError] = useState<string | null>(null);
   const [filters, setFilters] = useState<any>({});
   const [searchQuery, setSearchQuery] = useState<string>("");
-
-  const { addCondition, addGroup, removeItem, updateCondition } =
-    useFilterFunctions({ setFilter: setFilters });
 
   const search = (query: string) => {
     setSearchQuery(query);
@@ -34,38 +25,21 @@ export const LensProviderBase: React.FC<LensProviderProps> = ({
   const {
     data: config,
     isFetched,
-    // isError,
+    isError,
   } = useFetchConfig({ configEndpoint: endpoint });
-
-  const { flatData, fetchNextPage, isFetching, hasNextPage, refetch } =
-    useInfiniteFetch({
-      queryKey: endpoint,
-      globalFilter: searchQuery,
-      dataEndpoint: endpoint,
-      // keepPreviousData,
-      // dataCallback,
-      body: { filters: getProcessedFilters(filters) },
-      headers: permissionHeaders,
-    });
 
   return (
     <LensContext.Provider
       value={{
-        data,
-        isLoading,
         error,
         filters,
         search,
         config,
-        addCondition,
-        addGroup,
-        removeItem,
-        updateCondition,
-        flatData,
-        fetchNextPage,
-        isFetching,
-        hasNextPage,
-        refetch,
+        isFetched,
+        isError,
+        endpoint,
+        searchQuery,
+        lensConfiguration,
       }}
     >
       {children}
