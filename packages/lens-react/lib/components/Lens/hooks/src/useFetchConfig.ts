27cee3c84e2a2ea6423f7d65fc33b62d25960c82
ShreyDhyani <shrey@betalectic.com>
2025-02-18 17:42:06 +0530

    chore: revert to original

--- packages/lens-react/lib/components/Lens/hooks/src/useFetchConfig.ts
+++ packages/lens-react/lib/components/Lens/hooks/src/useFetchConfig.ts
@@ -1,20 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 
-export interface UseFetchConfigProps {
-  configEndpoint: string;
-  configCallback?: () => any;
-}
-
-const useFetchConfig = ({
-  configEndpoint,
-  configCallback,
-}: UseFetchConfigProps) => {
+const useFetchConfig = (configEndpoint: string, configCallback?: () => any) => {
   if (!configCallback && !configEndpoint) {
     throw new Error("Either configCallback or configEndpoint must be provided");
   }
-  const endpoint = configEndpoint + "/config";
   const configCallerFunction = async () => {
-    const response = await fetch(endpoint, {
+    const response = await fetch(configEndpoint, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
