647c30be623a8bc855170947fa774f8414485e72
ShreyDhyani <shrey@betalectic.com>
2025-02-19 16:47:55 +0530

    chore: create function for fetching column pining  styles

---
+++ packages/lens-react/lib/components/Datatable/hooks/getColumnPinningStyles.ts
@@ -0,0 +1,26 @@
+import { CSSProperties } from "react";
+import type { Column } from "@tanstack/react-table";
+
+const getColumnPinningStyles = (column: Column<any>): CSSProperties => {
+  const isPinned = column.getIsPinned();
+  const isLastLeftPinnedColumn =
+    isPinned === "left" && column.getIsLastColumn("left");
+  const isFirstRightPinnedColumn =
+    isPinned === "right" && column.getIsFirstColumn("right");
+
+  return {
+    boxShadow: isLastLeftPinnedColumn
+      ? "-4px 0 4px -4px gray inset"
+      : isFirstRightPinnedColumn
+      ? "4px 0 4px -4px gray inset"
+      : undefined,
+    left: isPinned === "left" ? `${column.getStart("left")}px` : undefined,
+    right: isPinned === "right" ? `${column.getAfter("right")}px` : undefined,
+    opacity: isPinned ? 0.95 : 1,
+    position: isPinned ? "sticky" : "relative",
+    // width: column.getSize(),
+    zIndex: isPinned ? 1 : 0,
+  };
+};
+
+export { getColumnPinningStyles };
