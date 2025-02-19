e3d80c8f8847041965e5a345cd7fde650579d149
ShreyDhyani <shrey@betalectic.com>
2025-02-19 12:43:23 +0530

    chore: removed packages

--- packages/lens-react/lib/main.ts
+++ packages/lens-react/lib/main.ts
@@ -3,8 +3,6 @@ export { Input } from "./components/Input";
 export { Label } from "./components/Label";
 export { Field } from "./components/Field";
 export { Checkbox } from "./components/Checkbox";
-export { Lens } from "./components/Lens";
-export { FilterBuilder } from "./components/Filters";
 
 export {
   LensProvider,
