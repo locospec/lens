e4a3d0f86cbc5f592df2754f3636dcae77285297
ShreyDhyani <shrey@betalectic.com>
2025-02-24 14:13:06 +0530

    chore: fix import issue

--- packages/lens-react/lib/hooks/index.ts
+++ packages/lens-react/lib/hooks/index.ts
@@ -1,4 +1,3 @@
-export { useInfiniteFetch } from "./src/useInfiniteFetch";
 export { useDebouncedEffect } from "./src/useDebouncedEffect";
 export { useEffectAfterMount } from "./src/useEffectAfterMount";
 export { useDebouncedEffectAfterMount } from "./src/useDebouncedEffectAfterMount";
