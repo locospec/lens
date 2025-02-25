75263be30299b1cb091709dbefd5260d877480d4
ShreyDhyani <shrey@betalectic.com>
2025-02-25 11:47:11 +0530

    chore: added raw data display for view type raw

--- packages/lens-react/lib/components/Views/ViewRenderer/ViewsRenderer.tsx
+++ packages/lens-react/lib/components/Views/ViewRenderer/ViewsRenderer.tsx
@@ -1,5 +1,11 @@
 import { Tabs } from "@/base/components/ui/tabs";
-import { Datatable, LensContext, SimpleFilters, View } from "@/main";
+import {
+  Datatable,
+  LensContext,
+  RawDisplay,
+  SimpleFilters,
+  View,
+} from "@/main";
 import { useContext, useState } from "react";
 import CustomSearchInput from "../../../../src/examples/components/CustomSearch";
 import { Card } from "@/base/components/ui/card";
@@ -9,6 +15,8 @@ import {
   ModifiedTabsList,
   ModifiedTabsTrigger,
 } from "@/base/components/ui/modified-tabs";
+import { Button } from "@/base/components/ui/button";
+import { Settings } from "lucide-react";
 
 const ViewsRenderer = () => {
   const lensContext = useContext(LensContext);
@@ -34,31 +42,43 @@ const ViewsRenderer = () => {
       {tabsList.length > 0 ? (
         <div className="h-fit bg-blue-50">
           <Tabs defaultValue="default" className="w-full h-full">
-            <ModifiedTabsList className="flex gap-x-4">
-              {tabsList.map((tab: any) => {
-                return (
-                  <ModifiedTabsTrigger
-                    key={tab.key}
-                    value={tab.key}
-                    className={cn(activeTab === tab.key ? "px-2 bg-white" : "")}
-                    onClick={() => {
-                      setActiveTab(tab.key);
-                    }}
-                  >
-                    {tab.config.view_name}
-                  </ModifiedTabsTrigger>
-                );
-              })}
-              <ModifiedTabsTrigger
-                value="add"
-                onClick={() => setActiveTab("add")}
-                className={cn(activeTab === "add" ? "px-2 bg-white" : "")}
-              >
-                +
-              </ModifiedTabsTrigger>
-            </ModifiedTabsList>
+            <div className="flex w-full justify-between">
+              <ModifiedTabsList className="flex gap-x-4">
+                {tabsList.map((tab: any) => {
+                  return (
+                    <ModifiedTabsTrigger
+                      key={tab.key}
+                      value={tab.key}
+                      className={cn(
+                        activeTab === tab.key ? "px-2 bg-white" : ""
+                      )}
+                      onClick={() => {
+                        setActiveTab(tab.key);
+                      }}
+                    >
+                      {tab.config.view_name}
+                    </ModifiedTabsTrigger>
+                  );
+                })}
+                <ModifiedTabsTrigger
+                  value="add"
+                  onClick={() => setActiveTab("add")}
+                  className={cn(activeTab === "add" ? "px-2 bg-white" : "")}
+                >
+                  +
+                </ModifiedTabsTrigger>
+              </ModifiedTabsList>
+              <div className="flex items-center justify-center">
+                <Button variant={"outline"} onClick={() => {}}>
+                  <Settings />
+                  Customise
+                </Button>
+              </div>
+            </div>
             {tabsList.map((tab: any) => {
               const default_scopes = tab.config?.scope?.filters;
+              const type = tab.config.type;
+              console.log(">>>>>>>", type);
               return (
                 <ModifiedTabsContent
                   className={cn(activeTab === tab.key ? "" : "hidden")}
@@ -71,15 +91,21 @@ const ViewsRenderer = () => {
                       <SimpleFilters defaultFiltersValue={default_scopes} />
                     </div>
                     <div className="h-[400px]">
-                      <Datatable
-                        key={tab.key}
-                        viewId={tab.key}
-                        onSelect={() => {}}
-                        selectedItems={[]}
-                        classNames={{
-                          row: "hover:bg-blue-100",
-                        }}
-                      />
+                      {type === "table" ? (
+                        <Datatable
+                          key={tab.key}
+                          viewId={tab.key}
+                          onSelect={() => {}}
+                          selectedItems={[]}
+                          classNames={{
+                            row: "hover:bg-blue-100",
+                          }}
+                        />
+                      ) : type === "raw" ? (
+                        <RawDisplay />
+                      ) : (
+                        <></>
+                      )}
                     </div>
                   </View>
                 </ModifiedTabsContent>
