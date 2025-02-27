df27e69a5fb29a03cc60bbf72a782ef0819ff072
ShreyDhyani <shrey@betalectic.com>
2025-02-27 10:09:22 +0530

    chore: added interface for views renderer

--- packages/lens-react/lib/components/Views/ViewRenderer/ViewsRenderer.tsx
+++ packages/lens-react/lib/components/Views/ViewRenderer/ViewsRenderer.tsx
@@ -21,7 +21,9 @@ import FiltersDialog from "./FiltersDialog";
 
 const DEFAULT_TAB = "default";
 
-const ViewsRenderer = () => {
+interface ViewsRendererInterface {}
+
+const ViewsRenderer: React.FC<ViewsRendererInterface> = ({}) => {
   const lensContext = useContext(LensContext);
 
   if (!lensContext) {
@@ -41,84 +43,82 @@ const ViewsRenderer = () => {
   };
 
   return (
-    <>
-      <Dialog>
-        {tabsList.length > 0 ? (
-          <Tabs
-            defaultValue={DEFAULT_TAB}
-            className="w-full h-full flex flex-col gap-y-3"
-          >
-            <div className="flex w-full justify-between">
-              <TabsList className="flex gap-x-4">
-                {tabsList.map((tab: any) => {
-                  return (
-                    <TabsTrigger
-                      key={tab.key}
-                      value={tab.key}
-                      onClick={() => {
-                        setActiveTab(tab.key);
-                      }}
-                    >
-                      {tab.config.view_name}
-                    </TabsTrigger>
-                  );
-                })}
-                <TabsTrigger
-                  value="add"
-                  onClick={() => {
-                    setActiveTab("add");
-                  }}
-                >
-                  +
-                </TabsTrigger>
-              </TabsList>
-              <FiltersTriggerButton
-                toggleShowSheet={toggleShowSheet}
-                activeTab={activeTab}
-              />
-            </div>
-            {tabsList.map((tab: any) => {
-              const type = tab.config.type;
-              return (
-                <TabsContent key={tab.key} value={tab.key}>
-                  <View
+    <Dialog>
+      {tabsList.length > 0 ? (
+        <Tabs
+          defaultValue={DEFAULT_TAB}
+          className="w-full h-full flex flex-col gap-y-3"
+        >
+          <div className="flex w-full justify-between">
+            <TabsList className="flex gap-x-4">
+              {tabsList.map((tab: any) => {
+                return (
+                  <TabsTrigger
                     key={tab.key}
-                    viewId={tab.key}
-                    showSheetProp={showSheets[tab.key]}
-                    setShowSheetProp={() => toggleShowSheet(tab.key)}
+                    value={tab.key}
+                    onClick={() => {
+                      setActiveTab(tab.key);
+                    }}
                   >
-                    <FiltersDialog name={tab.config.view_name} />
-                    <div className="flex py-4 items-center">
-                      <SearchInput />
-                      <SimpleFilters />
-                    </div>
-                    <div className="h-[400px]">
-                      {type === "table" ? (
-                        <Datatable
-                          key={tab.key}
-                          onSelect={() => {}}
-                          selectedItems={[]}
-                          classNames={{
-                            row: "hover:bg-blue-100",
-                          }}
-                        />
-                      ) : type === "raw" ? (
-                        <RawDisplay />
-                      ) : (
-                        <></>
-                      )}
-                    </div>
-                  </View>
-                </TabsContent>
-              );
-            })}
-            <AddViewTab />
-          </Tabs>
-        ) : (
-          <></>
-        )}
-      </Dialog>
-    </>
+                    {tab.config.view_name}
+                  </TabsTrigger>
+                );
+              })}
+              <TabsTrigger
+                value="add"
+                onClick={() => {
+                  setActiveTab("add");
+                }}
+              >
+                +
+              </TabsTrigger>
+            </TabsList>
+            <FiltersTriggerButton
+              toggleShowSheet={toggleShowSheet}
+              activeTab={activeTab}
+            />
+          </div>
+          {tabsList.map((tab: any) => {
+            const type = tab.config.type;
+            return (
+              <TabsContent key={tab.key} value={tab.key}>
+                <View
+                  key={tab.key}
+                  viewId={tab.key}
+                  showSheetProp={showSheets[tab.key]}
+                  setShowSheetProp={() => toggleShowSheet(tab.key)}
+                >
+                  <FiltersDialog name={tab.config.view_name} />
+                  <div className="flex py-4 items-center">
+                    <SearchInput />
+                    <SimpleFilters />
+                  </div>
+                  <div className="h-[400px]">
+                    {type === "table" ? (
+                      <Datatable
+                        key={tab.key}
+                        onSelect={() => {}}
+                        selectedItems={[]}
+                        classNames={{
+                          row: "hover:bg-blue-100",
+                        }}
+                      />
+                    ) : type === "raw" ? (
+                      <RawDisplay />
+                    ) : (
+                      <></>
+                    )}
+                  </div>
+                </View>
+              </TabsContent>
+            );
+          })}
+          <AddViewTab />
+        </Tabs>
+      ) : (
+        <></>
+      )}
+    </Dialog>
   );
 };
 ViewsRenderer.displayName = "ViewsRenderer";
