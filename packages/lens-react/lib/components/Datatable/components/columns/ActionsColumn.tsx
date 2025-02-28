5c8001cb9e27676dce240a81a79a5241b82aabd1
ShreyDhyani <shrey@betalectic.com>
2025-02-28 13:37:08 +0530

    refactor: Added actions column object separataly

---
+++ packages/lens-react/lib/components/Datatable/components/columns/ActionsColumn.tsx
@@ -0,0 +1,26 @@
+import { ActionsConfig } from "../../interface/DatatableInterface";
+import { RowInterface } from "./SelectionColumn";
+import { ActionsRenderer as actionsRenderer } from "../actions/ActionsRenderer";
+import { metaReader } from "../../utils/metaReader";
+
+const ActionsColumn = (actions: ActionsConfig) => {
+  return {
+    id: actions.header,
+    accessorKey: actions.header,
+    meta: metaReader(actions),
+    header: actions.header,
+    cell: ({ row }: RowInterface) => {
+      return actionsRenderer({
+        actions: actions.options,
+        row: row.original,
+      });
+    },
+    enableSorting: false,
+    enableHiding: false,
+    size: actions?.width || 140,
+    minSize: actions?.minWidth || 100,
+    maxSize: actions?.maxWidth || 800,
+  };
+};
+
+export default ActionsColumn;
