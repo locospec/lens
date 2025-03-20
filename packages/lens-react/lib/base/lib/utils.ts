efb8525a7bb433a45c18114a92183f7b8d833f77
ShreyDhyani <shrey@betalectic.com>
2025-03-20 13:55:46 +0530

    chore: removed comments

--- packages/lens-react/lib/base/lib/utils.ts
+++ packages/lens-react/lib/base/lib/utils.ts
@@ -1,9 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
-// import { extendTailwindMerge } from "tailwind-merge";
-
-// const customTwMerge = extendTailwindMerge({ prefix: "le" });
 
 export function cn(...inputs: ClassValue[]) {
-  // return customTwMerge(clsx(inputs));
   return clsx(inputs);
 }
