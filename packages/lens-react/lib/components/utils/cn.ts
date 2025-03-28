80bbb08c5647b011fccbc019e32e488ea42560dc
ShreyDhyani <shrey@betalectic.com>
2025-03-28 18:28:16 +0530

    chore: added twMerge for lens cn function

--- packages/lens-react/lib/components/utils/cn.ts
+++ packages/lens-react/lib/components/utils/cn.ts
@@ -1,9 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
-// import { extendTailwindMerge } from "tailwind-merge";
-
-// const customTwMerge = extendTailwindMerge({ prefix: "" });
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  // return customTwMerge(clsx(inputs));
-  return clsx(inputs);
+  return twMerge(clsx(inputs));
 }
