c268acf3e0e015909abc2e2a88cb82cf7df2a6de
ShreyDhyani <shrey@betalectic.com>
2025-04-09 10:40:41 +0530

    chore: modified provider example

--- packages/lens-react/src/examples/ProviderExample.tsx
+++ packages/lens-react/src/examples/ProviderExample.tsx
@@ -79,54 +79,54 @@ const ProviderExample = () => {
           </div>
           <div className="h-[400px] px-4">
             <Datatable
-              selectedItems={checkedIds}
-              onSelect={handleSelectionChange}
-              classNames={{
-                wrapper: "border border-[#eee]",
-                headers:
-                  "flex gap-x-[14px] pl-[14px] pr-[50px] py-[15px] border-0",
-                header: "leading-[16px] p-0",
-                resizehandle: "bg-gray-100 hover:bg-gray-300",
-                row: "flex gap-x-[14px] pl-[17px] pr-[31px] pt-[15px] pb-4 border-b border-[#eee] hover:bg-gray-50",
-                cell: "p-0 border-r-0",
-                actionsCell: "p-0 gap-x-4 leading-3 justify-end",
-                actionsHeader: "justify-end text-end text-right pr-2",
-              }}
-              rowActions={{
-                cities: (rowData: Record<string, any>) => {
-                  console.log(rowData);
-                },
-                id: (rowData: Record<string, any>) => {
-                  console.log("FROM ID COLUMNS", rowData);
-                },
-              }}
-              actionsMapping={{
-                sample: {
-                  url: "/sample/:id/:city.locality.id",
-                  icon: Clock,
-                  confirmation: true,
-                  callback: ({ url, data }: CallbackInterface) => {
-                    console.log("Sample Callback ", url, data);
-                  },
-                },
-                edit: {
-                  icon: Edit3Icon,
-                  styles:
-                    "font-bold bg-blue-300 px-2 rounded-full hover:bg-blue-500 text-red-400 font-bold hover:text-red-500",
-                },
-                sample2: {
-                  component: (
-                    <div
-                      className="flex items-center gap-x-2 rounded-full bg-red-200 hover:bg-red-300 cursor-pointer p-1"
-                      onClick={() => {
-                        console.log(">> CLICKED ON ACTIONS COMPONENT");
-                      }}
-                    >
-                      <AArrowDown />
-                    </div>
-                  ),
-                },
-              }}
+            // selectedItems={checkedIds}
+            // onSelect={handleSelectionChange}
+            // classNames={{
+            //   wrapper: "border border-[#eee]",
+            //   headers:
+            //     "flex gap-x-[14px] pl-[14px] pr-[50px] py-[15px] border-0",
+            //   header: "leading-[16px] p-0",
+            //   resizehandle: "bg-gray-100 hover:bg-gray-300",
+            //   row: "flex gap-x-[14px] pl-[17px] pr-[31px] pt-[15px] pb-4 border-b border-[#eee] hover:bg-gray-50",
+            //   cell: "p-0 border-r-0",
+            //   actionsCell: "p-0 gap-x-4 leading-3 justify-end",
+            //   actionsHeader: "justify-end text-end text-right pr-2",
+            // }}
+            // rowActions={{
+            //   cities: (rowData: Record<string, any>) => {
+            //     console.log(rowData);
+            //   },
+            //   id: (rowData: Record<string, any>) => {
+            //     console.log("FROM ID COLUMNS", rowData);
+            //   },
+            // }}
+            // actionsMapping={{
+            //   sample: {
+            //     url: "/sample/:id/:city.locality.id",
+            //     icon: Clock,
+            //     confirmation: true,
+            //     callback: ({ url, data }: CallbackInterface) => {
+            //       console.log("Sample Callback ", url, data);
+            //     },
+            //   },
+            //   edit: {
+            //     icon: Edit3Icon,
+            //     styles:
+            //       "font-bold bg-blue-300 px-2 rounded-full hover:bg-blue-500 text-red-400 font-bold hover:text-red-500",
+            //   },
+            //   sample2: {
+            //     component: (
+            //       <div
+            //         className="flex items-center gap-x-2 rounded-full bg-red-200 hover:bg-red-300 cursor-pointer p-1"
+            //         onClick={() => {
+            //           console.log(">> CLICKED ON ACTIONS COMPONENT");
+            //         }}
+            //       >
+            //         <AArrowDown />
+            //       </div>
+            //     ),
+            //   },
+            // }}
             />
           </div>
         </View>
