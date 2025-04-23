a85412e85c3d33766a3e148385936452ed0bed05
ShreyDhyani <shrey@betalectic.com>
2025-04-23 18:37:44 +0530

    chore: rmoved blueprint-react package

--- packages/blueprint-react/lib/components/Input/index.tsx
+++
@@ -1,6 +0,0 @@
-export function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
-  const { className, ...restProps } = props;
-  console.log("Blueprint Input");
-
-  return <input className={`${className}`} {...restProps} />;
-}
