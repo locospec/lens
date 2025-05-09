9dba7aa8749548781338374cbf6a8e51d83fbf66
ShreyDhyani <shrey@betalectic.com>
2025-05-09 17:34:59 +0530

    chore: moved unused code to bottom

--- packages/lens-react/lib/components/Sheet/sheets/DefaultSheet.tsx
+++ packages/lens-react/lib/components/Sheet/sheets/DefaultSheet.tsx
@@ -18,19 +18,7 @@ const DefaultSheet = ({ setCurrentSheet }: DefaultSheetInterface) => {
       <SheetHeader>
         <SheetTitle>{"Customise Views"}</SheetTitle>
       </SheetHeader>
-      <div className="flex flex-col gap-2 pt-4">
-        <OptionWrapper
-          callback={() => {
-            setCurrentSheet("layout_options");
-          }}
-        >
-          <div className="flex gap-x-2 text-sm items-center cursor-pointer">
-            <List className="text-[var(--gray-9)]" size={14} />
-            <label className="cursor-pointer">Layout Options</label>
-          </div>
-          <ChevronRight size={15} className="text-[var(--gray-9)]" />
-        </OptionWrapper>
-        <div className="w-full h-[1px] bg-[var(--gray-7)]" />
+      <div className="flex flex-col gap-2 px-4">
         <OptionWrapper
           callback={() => {
             setCurrentSheet("field_options");
@@ -48,3 +36,19 @@ const DefaultSheet = ({ setCurrentSheet }: DefaultSheetInterface) => {
 };
 
 export default DefaultSheet;
+
+// Code For Separator
+/* <div className="w-full h-[1px] bg-gray-300" /> */
+
+// Code for Layout Ouptions
+/* <OptionWrapper
+      callback={() => {
+        setCurrentSheet("layout_options");
+      }}
+    >
+      <div className="flex gap-x-2 text-sm items-center cursor-pointer">
+        <List className="text-[var(--gray-9)]" size={14} />
+        <label className="cursor-pointer">Layout Options</label>
+      </div>
+      <ChevronRight size={15} className="text-[var(--gray-9)]" />
+    </OptionWrapper> */
