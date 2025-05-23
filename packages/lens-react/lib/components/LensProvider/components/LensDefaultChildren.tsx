0531b711f2d381d65f0e2d789c30d4c9b9de8b59
ShreyDhyani <shrey@betalectic.com>
2025-05-23 13:48:45 +0530

    chore: modified search input and searchbar placement

--- packages/lens-react/lib/components/LensProvider/components/LensDefaultChildren.tsx
+++ packages/lens-react/lib/components/LensProvider/components/LensDefaultChildren.tsx
@@ -6,7 +6,7 @@ const LensDefaultChildren: React.FC<LensDefaultChildrenInterface> = () => {
   return (
     <View>
       <div className="flex h-full flex-col gap-y-4 px-4 py-2">
-        <div className="flex items-center justify-between gap-x-2">
+        <div className="flex items-start justify-between gap-x-2">
           <div className="flex flex-col gap-y-2">
             <SearchInput />
           </div>
