c968a95841970edca9ffd938e3ed3664a3fc1098
ShreyDhyani <shrey@betalectic.com>
2025-05-26 12:02:40 +0530

    chore: modified styling for EnumInput popover trigger to match with search input

--- packages/lens-react/lib/components/EnumInput/utils/src/generateStylingClasses.ts
+++ packages/lens-react/lib/components/EnumInput/utils/src/generateStylingClasses.ts
@@ -1,12 +1,17 @@
+import { cn } from "@lens/base/lib/utils";
+
 const generateStylingClasses = (classNamesObject: Record<string, string>) => {
-  const enumClasses = classNamesObject?.enum || "";
-  const popoverWrapperClasses = classNamesObject?.popoverWrapper || "";
-  const popoverClasses = classNamesObject?.popover || "";
-  const searchInputWrapperClasses = classNamesObject?.searchInputWrapper || "";
-  const searchIconClasses = classNamesObject?.searchIcon || "";
-  const searchInputClasses = classNamesObject?.searchInput || "";
-  const itemClasses = classNamesObject?.items || "";
-  const separatorClasses = classNamesObject?.separator || "";
+  const enumClasses = cn("", classNamesObject?.enum);
+  const popoverWrapperClasses = cn("", classNamesObject?.popoverWrapper);
+  const popoverClasses = cn("", classNamesObject?.popover);
+  const searchInputWrapperClasses = cn(
+    "",
+    classNamesObject?.searchInputWrapper
+  );
+  const searchIconClasses = cn("", classNamesObject?.searchIcon);
+  const searchInputClasses = cn("", classNamesObject?.searchInput);
+  const itemClasses = cn("", classNamesObject?.items);
+  const separatorClasses = cn("", classNamesObject?.separator);
 
   return {
     enumClasses,
