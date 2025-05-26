d7e2196855aba7f4424a18c6d95a1fdf09ce5644
ShreyDhyani <shrey@betalectic.com>
2025-05-26 15:43:27 +0530

    chore: formatting

--- packages/lens-react/lib/components/ChipFilter/ChipFilter.tsx
+++ packages/lens-react/lib/components/ChipFilter/ChipFilter.tsx
@@ -1,11 +1,11 @@
-import React, { useMemo, useRef } from "react";
-import type { Condition } from "../Filters";
 import { cn } from "@lens/base/lib/utils";
 import { X } from "lucide-react";
-import ChipOP from "./ChipOp";
+import React, { useMemo, useRef } from "react";
+import type { Condition } from "../Filters";
 import { returnOperators } from "../Filters/constants";
 import SelectOptions from "../SelectOptions/SelectOptions";
 import { useSimpleFiltersContext } from "../SimpleFilters/context/useSimpleFiltersContext";
+import ChipOP from "./ChipOp";
 
 const COMMON_RENDERER_CLASSES =
   "text-xs cursor-pointer bg-white w-fit h-full flex items-center px-1.5 py-1 first:rounded-l-lg last:rounded-r-lg text-gray-700";
@@ -65,7 +65,7 @@ const ChipFilter: React.FC<ChipFilterInteface> = ({
   return (
     <div
       className={cn(
-        "relative border border-gray-300 rounded-lg bg-gray-200 flex items-center gap-x-px! py-0 px-0 h-6 w-fit shrink-0"
+        "gap-x-px! relative flex h-6 w-fit shrink-0 items-center rounded-lg border border-gray-300 bg-gray-200 px-0 py-0"
       )}
       ref={ref}
     >
@@ -88,7 +88,7 @@ const ChipFilter: React.FC<ChipFilterInteface> = ({
       />
       {showClose && (
         <div
-          className={cn(COMMON_RENDERER_CLASSES, "rounded-r-lg text-xs px-1")}
+          className={cn(COMMON_RENDERER_CLASSES, "rounded-r-lg px-1 text-xs")}
           onClick={clearValue}
         >
           <X size={12} />
