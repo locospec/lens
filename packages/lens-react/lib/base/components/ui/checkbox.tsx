5da6ba12d6e0c84091e57f96c75e304f935640dc
ShreyDhyani <shrey@betalectic.com>
2025-05-27 17:04:19 +0530

    chore: added radio styled checkbox for single selction table

--- packages/lens-react/lib/base/components/ui/checkbox.tsx
+++ packages/lens-react/lib/base/components/ui/checkbox.tsx
@@ -1,28 +1,42 @@
 "use client";
 
-import * as React from "react";
+import { cn } from "@lens/base/lib/utils";
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import { CheckIcon } from "lucide-react";
-import { cn } from "@lens/base/lib/utils";
+import * as React from "react";
+
+interface CustomCheckboxProps
+  extends React.ComponentProps<typeof CheckboxPrimitive.Root> {
+  asRadio?: boolean;
+}
 
 function Checkbox({
   className,
+  asRadio = false,
   ...props
-}: React.ComponentProps<typeof CheckboxPrimitive.Root>) {
+}: CustomCheckboxProps) {
   return (
     <CheckboxPrimitive.Root
       data-slot="checkbox"
       className={cn(
-        "peer border-input dark:bg-input/30 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground dark:data-[state=checked]:bg-primary data-[state=checked]:border-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive size-4 shrink-0 rounded-[4px] border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
+        "border-input dark:bg-input/30 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground dark:data-[state=checked]:bg-primary data-[state=checked]:border-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive shadow-xs peer size-4 shrink-0 rounded-[4px] border outline-none transition-all focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props}
     >
       <CheckboxPrimitive.Indicator
         data-slot="checkbox-indicator"
-        className="flex items-center justify-center text-current transition-none w-full h-full"
+        className="flex h-full w-full items-center justify-center text-current transition-none"
       >
-        <CheckIcon className="size-3" />
+        {asRadio ? (
+          <div
+            className={cn(
+              "data-[state=checked]:bg-primary-foreground h-1/2 w-1/2 rounded-full bg-white transition-all"
+            )}
+          />
+        ) : (
+          <CheckIcon className="size-3" />
+        )}
       </CheckboxPrimitive.Indicator>
     </CheckboxPrimitive.Root>
   );
