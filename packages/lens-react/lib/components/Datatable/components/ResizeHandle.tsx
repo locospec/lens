cd574da8b4d133a5595c8b28bc4e7b42d9e00987
ShreyDhyani <shrey@betalectic.com>
2025-06-11 16:24:01 +0530

    chore: Removed unused imports

--- packages/lens-react/lib/components/Datatable/components/ResizeHandle.tsx
+++ packages/lens-react/lib/components/Datatable/components/ResizeHandle.tsx
@@ -1,13 +1,10 @@
 import { cn } from "@lens/components/utils/cn";
 import type { Header } from "@tanstack/react-table";
-import React from "react";
 import { useDatatableContext } from "../context/useDatatableContext.ts";
-import { head } from "lodash";
 
 export interface ResizeHandleProps {
   header: Header<any, unknown>;
   isResizing: boolean;
-  setIsInResizeArea: React.Dispatch<React.SetStateAction<boolean>>;
   disabled?: boolean;
   isLast?: boolean;
 }
@@ -15,7 +12,6 @@ export interface ResizeHandleProps {
 const ResizeHandle = ({
   header,
   isResizing,
-  setIsInResizeArea,
   disabled = false,
   isLast = false,
 }: ResizeHandleProps) => {
@@ -28,7 +24,7 @@ const ResizeHandle = ({
       onMouseDown={resizeHandle}
       onTouchStart={resizeHandle}
       className={cn(
-        "absolute top-0 right-0 z-20 flex h-full w-1 touch-none items-center justify-end select-none",
+        "absolute right-0 top-0 z-20 flex h-full w-1 touch-none select-none items-center justify-end",
         disabled ? "cursor-not-allowed" : "cursor-ew-resize"
       )}
       data-isresising={isResizing ? "true" : "false"}
@@ -37,7 +33,7 @@ const ResizeHandle = ({
       <div
         className={cn(
           disabled ? "hidden" : "opacity-100",
-          "h-1/2 w-0.5 rounded-l-md transition-all duration-200 ease-in-out transform hover:scale-y-120 hover:scale-x-120 hover:origin-center",
+          "hover:scale-y-120 hover:scale-x-120 h-1/2 w-0.5 transform rounded-l-md transition-all duration-200 ease-in-out hover:origin-center",
           "bg-gray-300 hover:bg-gray-600",
           "dark:bg-gray-500 dark:hover:bg-gray-300",
           header.column.getIsResizing() && "bg-gray-700",
